11d41a314824fa308e6a18ffac92ad74dba71d01
SikPang <87380790+SikPang@users.noreply.github.com>
2022-08-10 15:36:56 +0900

    Enemy Animation & Flip

--- Assets/Scripts/Character/Enemy/EnemyMove.cs
+++ Assets/Scripts/Character/Enemy/EnemyMove.cs
@@ -8,17 +8,25 @@ public class EnemyMove : MonoBehaviour
     [SerializeField] Transform player;
     Enemy character;
     Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         character = GetComponent<Enemy>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        Vector2 direction = player.position - transform.position;
-        rigidBody.MovePosition(rigidBody.position + direction.normalized * character.GetSpeed() * Time.deltaTime);
+        Vector2 direction = (player.position - transform.position).normalized;
+        rigidBody.MovePosition(rigidBody.position + direction * character.GetSpeed() * Time.deltaTime);
+
+        if (direction.x >= 0)
+            spriteRenderer.flipX = false;
+        else
+            spriteRenderer.flipX = true;
+
         //transform.Translate((Vector2)direction.normalized * character.GetSpeed()/10f * Time.deltaTime);
     }
 }
