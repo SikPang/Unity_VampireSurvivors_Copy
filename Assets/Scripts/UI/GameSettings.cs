e804b3f8ef1f971c94eaf34b863d89faf9ac27ec
SikPang <87380790+SikPang@users.noreply.github.com>
2022-08-11 19:02:38 +0900

    GameSetting

---
+++ Assets/Scripts/UI/GameSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.SetResolution(1280, 720, false);
+    }
+}
